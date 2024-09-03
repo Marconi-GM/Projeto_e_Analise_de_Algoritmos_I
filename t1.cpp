7f966f500d7b7dffcc6e90d85d66351ce14046e9
Guilherme Marconi Mugnai <g241277@dac.unicamp.br>
2024-09-03 15:50:19 -0300

    termina de fazer as funções mas ainda não passa em varioes  testes, verificar as lógicas

--- t1.cpp
+++ t1.cpp
@@ -5,56 +5,50 @@
 #include <algorithm>
 #include <cmath>
 
-int pertinencia(int *S, int *n, int *set, int *pos, int numero) {
+int pertinencia(int *n, int *set, int *pos, int numero) {
     /* Devolve 1 se numero ∈ S e zero caso contrário*/
+    for(int i = 0; i < *n; i++) {
+        if(set[i] == numero && set[pos[numero]] == numero)
+        {
+            return 1;
+        }
+    }
+
     return 0;
 }
 
-void insere_valor(int *S, int *n, int *set, int *pos, int numero) {
+void insere_valor(int *n, int *set, int *pos, int numero) {
     /* Insere numero em S caso ele ainda não esteja no subcojunto. Não faz nada
     caso contrário*/
-
+    if(pos[numero] == -1) {
+        set[*n] = numero;
+        pos[numero] = *n;
+        *n += 1;
+    }
 }
 
-void remove_valor(int *S, int *n, int *set, int *pos, int numero) {
+void remove_valor(int *n, int *set, int *pos, int numero) {
     /* remove numero de S caso ele esteja no subconjunto. Não faz nada caso contrário*/
+    if(pos[numero] != -1) {
+        // coloca o último elemento do vetor set na posição que teve seu elemento removido
+        // -1 acrescentado pois o vetor começa em zero
+        set[pos[numero]] = set[(*n) - 1];
+        // atualiza a posição nova do elemento que foi movido
+        pos[set[(*n) - 1]] = pos[numero];
+        // atualiza a posição do elemnto que foi removido para -1 pois ele não
+        // está mais nos array's
+        pos[numero] = -1;
+        *n -= 1;
+    }
 }
 
-void reinicializa_S(int *S, int *n) {
+void reinicializa_S(int *n) {
     /* Reinicializa o subconjunto S tornando-o vazio*/
+    *n = 0;
 }
 
 
 int main(void) {
-
-
-
-    // vou receber um vetor N (devo oderná-lo?)     
-    // esse vetor vai representar um conjunto
-    // e vou usar um vetor de bits para representar um subconjunto desse vetor
-    // nesse vetor Q[i] = 1 se i pertence a S e zero caso contrário
-
-    /*  Quero realizar as seguintes operações
-        1) devolver 1 se i estiver em [N]
-        2) inserir um elemento i caso ele não esteja em N
-        3) remover i caso ele esteja em N
-        4) reiniciar S como um conjunto vazio
-
-
-        teremos uma variável n que guarda o tamanho de S
-        um vetor set que é uma lista contendo os elementos em S (em qualquer ordem)
-        um vetor pos contendo a posição de cada elemento do vetor S
-            Para i ∈ S, pos[i] indica a posi¸c˜ao de i no vetor set. Se i̸ ∈ S, ent˜ao
-            pos[i] n˜ao tem significado nenhum.
-        
-        ou seja os vetores tem apontadores um para o outro
-
-        para inserir em S
-
-        set[n] <- i
-        pos[i] <- n
-        n <- n+1
-    */
     std::string operacao;
 
     int N = 0,
@@ -62,40 +56,45 @@ int main(void) {
         numero = 0,
         pertence = 0;
 
-    // receber N (representando o conjunto) e Q (número de operações)
+    // recebe N (representando o conjunto) e Q (número de operações)
     std::cin >> N >> Q;
 
-    int *S = new int,
-        *set = new int[N],
+    int *set = new int[N],
         *pos = new int[N],
         *n = new int;
 
+    *n = 0;
     std::fill(set, set+N, -1);
     std::fill(pos, pos+N, -1);
 
-/*  O vetor set ´e simplesmente uma lista contendo os elementos de S em
-alguma ordem (por exemplo, ordem de inser¸c˜ao).
-
-        • Para i ∈ S, pos[i] indica a posi¸c˜ao de i no vetor set. Se i̸ ∈ S, ent˜ao
-pos[i] n˜ao tem significado nenhum
-
-*/
-
     for(int i = 0; i < Q; i++) {
-        std::cin >> operacao >> numero;
+        std::cin >> operacao;
         if(operacao == "p") {
-            pertence = pertinencia(S, n, set, pos, numero);
+            std::cin >> numero;
+            pertence = pertinencia(n, set, pos, numero);
             if(pertence == 1) {
-                std::cout << 1;
+                std::cout << 1 << std::endl;
             } else {
-                std::cout << 0;
+                std::cout << 0 << std::endl;
             }
         } else if(operacao == "i") {
-            insere_valor(S, n, set, pos, numero);
+            std::cin >> numero;
+            insere_valor(n, set, pos, numero);
         } else if(operacao == "r") {
-            remove_valor(S, n, set, pos, numero);
+            std::cin >> numero;
+            remove_valor(n, set, pos, numero);
         } else if(operacao == "l") {
-            reinicializa_S(S, n);
+            reinicializa_S(n);
+        } else if(operacao == "print") {
+            for(int i = 0; i < N; i++)
+                std::cout << set[i] << " ";
+
+            std::cout << std::endl;
+            
+            for(int i = 0; i < N; i++)
+                std::cout << pos[i] << " ";
+            
+            std::cout << std::endl;
         }
     }
 
