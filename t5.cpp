984d68881ed44b5524b162c0ad85654ca88e074b
Guilherme Marconi Mugnai <g241277@dac.unicamp.br>
2024-11-01 14:55:52 -0300

    resolve o problema de tempo do git anterior, o problema era o acesso ao tipo de tuplas, então mudei as tuplas para vetores de inteiros e para os ordenar usei um quicksort

--- t5.cpp
+++ t5.cpp
@@ -5,44 +5,73 @@
 #include <vector>
 
 
-void ordena_intervalos(std::vector<std::tuple<int, int>>& intervalos){
-    std::sort(intervalos.begin(), intervalos.end(), [](const std::tuple<int, int>& a, const std::tuple<int, int>& b){
-        return std::get<1>(a) < std::get<1>(b);
-    });
+void troca(int *L, int *R, int i, int j) {
+    int temp = R[i];
+    R[i] = R[j];
+    R[j] = temp;
+
+    temp = L[i];
+    L[i] = L[j];
+    L[j] = temp;
 }
 
-// A ESCOLHA GULOSA É O INTERVALO COM O MAIOR NÚMERO DE TÉRMINO QUE COMEÇE EM 0 MAS TENHA
-// O MAIOR NÚMERO DE TÉRMINO POIS A INTENÇÃO É ABRANGER O MAIOR INTERVALO POSSÍVEL  
-/* A subestrutura  ́otima diz que se am pertence a uma solu ̧c ̃ao  ́otima A
-de Sk , ent ̃ao A  ́ tamu  ́e uma solu ̧c ̃ao  ́otima de Sm.
-*/
 
-/* A PRIMEIRA LINHA DA SAÍDA É O NÚMERO MÍNIMO DE INTERVALOS
+int particiona(int *L, int *R, int inicio, int fim) {
+    int pivo = R[fim];
+    int i = (inicio - 1);
 
-    AS T LINHAS SEGUINTES DEVEM CONTER CADA INTERVALO DA SOLUÇÃO ÓTIMA ORDENADOS
-    PELO VALOR Li (OS DOIS VALORES DEVEM SER SEPARADOS POR UM ESPAÇO)
-*/
-void imprime_intervalos(std::vector<std::tuple<int, int>> intervalos, int n){
-    for(int i = 0; i <= n; i++){
-        std::cout << std::get<0>(intervalos[i]) <<" "<< std::get<1>(intervalos[i]) << std::endl; 
+    for (int j = inicio; j < fim; j++) {
+        if (R[j] <= pivo) {
+            i++;
+            troca(L, R, i, j);
+        }
+    }
+
+    troca(L, R, i + 1, fim);
+    return (i + 1);
+}
+
+
+void quickSort(int *L, int *R, int inicio, int fim) {
+    if (inicio < fim) {
+        int pivo = particiona(L, R, inicio, fim);
+
+        quickSort(L, R, inicio, pivo - 1);
+        quickSort(L, R, pivo + 1, fim);
     }
 }
 
 
-int encontra_escolha_gulosa(std::vector<std::tuple<int, int>> intervalos, int indice_atual, int ini, int fim, int valor){
-    int indice = -1,
-        max = std::get<1>(intervalos[indice_atual]);
+void ordena_intervalos(int *L, int *R, int n) {
+    quickSort(L, R, 0, n - 1);
+}
 
-    for(long unsigned int i = indice_atual + 1 ; i < intervalos.size(); i++){
-        if(ini <= std::get<0>(intervalos[i]) && std::get<0>(intervalos[i]) <= fim && fim <= std::get<1>(intervalos[i]) && std::get<1>(intervalos[i]) >= max){
-            if(std::get<1>(intervalos[i]) == max){
-                if(std::get<0>(intervalos[i]) < std::get<0>(intervalos[indice])){
+
+int encontra_escolha_gulosa(int *L, int *R, int n, int indice_atual, int ini, int fim, int valor){
+    int indice = -1,
+        max = R[indice_atual];
+
+    // percorre todos os intervalos
+    for(int i = indice_atual + 1; i <= n; i++){
+        /*
+            ini <= L[i] && L[i] <= fim ---> se o intervalo atual abrange o intervalo
+            anterior
+
+            fim <= R[i] ---> se o intervalo atual tem ponto final maior que o 
+            intervalo anterior
+
+            R[i] => max ---> se o intervalo atual é o intervalo que maximiza a 
+            computação
+        */
+        if(ini <= L[i] && L[i] <= fim && fim <= R[i] && R[i] >= max){
+            if(R[i] == max){
+                if(L[i] < L[indice]){
                     indice = i;
-                    max = std::get<1>(intervalos[i]);
+                    max = R[i];
                 }
             } else{
                 indice = i;
-                max = std::get<1>(intervalos[i]);
+                max = R[i];
             }
         }
     }
@@ -50,20 +79,21 @@ int encontra_escolha_gulosa(std::vector<std::tuple<int, int>> intervalos, int in
     return indice;
 }
 
-int encontra_primeiro_intervalo(std::vector<std::tuple<int, int>> intervalos){
+int encontra_primeiro_intervalo(int *L, int *R, int n){
+    //  A função encontra o intervalo que tem menor valor em L[i] e maior em R[i]
     int indice = -1,
         max = -99999;
 
-    for(long unsigned int i = 1; i < intervalos.size() && std::get<0>(intervalos[i]) <= 0; i++){
-        if(std::get<0>(intervalos[i]) <= 0 && std::get<1>(intervalos[i]) >= max){
-            if(std::get<1>(intervalos[i]) == max){
-                if(std::get<0>(intervalos[i]) < std::get<0>(intervalos[indice])){
+    for(int i = 1; i <= n; i++){
+        if(L[i] <= 0 && R[i] >= max){
+            if(R[i] == max){
+                if(L[i] < L[indice]){
                     indice = i;
-                    max = std::get<1>(intervalos[i]);
+                    max = R[i];
                 }
             } else{
                 indice = i;
-                max = std::get<1>(intervalos[i]);
+                max = R[i];
             }
         }
     }
@@ -72,31 +102,28 @@ int encontra_primeiro_intervalo(std::vector<std::tuple<int, int>> intervalos){
 }
 
 
-std::vector<std::tuple<int, int>> encontra_solucao_otima(std::vector<std::tuple<int, int>> intervalos, int valor){
+std::vector<std::tuple<int, int>> encontra_solucao_otima(int *L, int *R, int n, int valor){
     int indice_escolha_gulosa = 0,
         inicio = 0,
         fim = 0;
     // vetor que contem os intervalos que compoem a resposta
     std::vector<std::tuple<int, int>> resposta;
-    // tupla que contem o intervalo que é a escolha gulosa da iteração
-    std::tuple<int, int> escolha_gulosa(0, 0);
+
     // encontra a primeira escolha gulosa
-    indice_escolha_gulosa = encontra_primeiro_intervalo(intervalos);
+    indice_escolha_gulosa = encontra_primeiro_intervalo(L, R, n);
+
     // caso a primeira escolha gulosa não seja um intervalo que não abrange o 0,
     // então é um intervalo inválido e logo recebe valor -1
     if(indice_escolha_gulosa != -1){
-        // valor de início do intervalo
-        // std::get<0> acessa o primeiro valor da tupla
-        inicio = std::get<0>(intervalos[indice_escolha_gulosa]);
-        // valor final do intervalo
-        // std::get<1> acessa o segundo valor da tupla
-        fim = std::get<1>(intervalos[indice_escolha_gulosa]);
-        // append do intervalo no vetor de repostas
-        resposta.push_back(intervalos[indice_escolha_gulosa]);
+        // valores de início e fim do intervalo
+        inicio = L[indice_escolha_gulosa];
+        fim = R[indice_escolha_gulosa];
+        resposta.push_back(std::make_tuple(L[indice_escolha_gulosa], R[indice_escolha_gulosa]));
+
         // enquanto o intervalo for válido e seu valor final não ultrapassar V, faça
-        while(indice_escolha_gulosa != -1 && std::get<1>(intervalos[indice_escolha_gulosa]) < valor){
+        while(indice_escolha_gulosa != -1 && R[indice_escolha_gulosa] < valor){
             // encontra a escolha gulosa da iteração
-            indice_escolha_gulosa = encontra_escolha_gulosa(intervalos, indice_escolha_gulosa, inicio, fim, valor);
+            indice_escolha_gulosa = encontra_escolha_gulosa(L, R, n, indice_escolha_gulosa, inicio, fim, valor);
             // se a escolha gulosa for inválida, ou seja, se a intersecção de
             // seu intervalo com o intervalo anterior é vazia
             if(indice_escolha_gulosa == -1){
@@ -104,9 +131,9 @@ std::vector<std::tuple<int, int>> encontra_solucao_otima(std::vector<std::tuple<
                 resposta.clear();
             } else{
                 // caso contrário atualiza os índices e armazena a resposta
-                inicio = std::get<0>(intervalos[indice_escolha_gulosa]);
-                fim = std::get<1>(intervalos[indice_escolha_gulosa]);
-                resposta.push_back(intervalos[indice_escolha_gulosa]);
+                inicio = L[indice_escolha_gulosa];
+                fim = R[indice_escolha_gulosa];
+                resposta.push_back(std::make_tuple(L[indice_escolha_gulosa], R[indice_escolha_gulosa]));
             }
         }
     }
@@ -116,31 +143,26 @@ std::vector<std::tuple<int, int>> encontra_solucao_otima(std::vector<std::tuple<
 
 
 int main(void){
-    // 1 <= V <= 50 000
     int V = 0;
-    // 1 <= n <= 500 000
     int n = 0;
-    // -10 000 <= L < R <= 10 000
-    int L = 0,
-        R = 0;
-    std::vector<std::tuple<int, int>> solucao_otima;
-    // recebe V e n 
+
     std::cin >> V >> n;
-    // cria um vetor de tuplas, de tamanho n
-    std::vector<std::tuple<int, int>> intervalos(n+1);
-    intervalos[0] = std::tuple<int, int>(0, 0);
-    // recebe cada intervalo e o armazena no vetor
+
+    int *L = new int[n+1],
+        *R = new int[n+1];
+    L[0] = 0;
+    R[0] = 0;
+
+    std::vector<std::tuple<int, int>> solucao_otima;
+
     for(int i = 1; i <= n; i++){
-        std::cin >> L >> R;
-        std::tuple<int, int> intervalo(L, R);
-        intervalos[i] = intervalo;
+        std::cin >> L[i] >> R[i];
+
     }
-
     // ordena os intervalos em ordem crescente de término do intervalo
-    ordena_intervalos(intervalos);
-    //imprime_intervalos(intervalos, n);
-    solucao_otima = encontra_solucao_otima(intervalos, V);
+    ordena_intervalos(L, R, n);
 
+    solucao_otima = encontra_solucao_otima(L, R , n, V);
     // imprime a saída
     if(solucao_otima.empty()){
         std::cout << 0 << std::endl;
